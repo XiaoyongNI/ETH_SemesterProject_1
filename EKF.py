6f4d0cf561f63509fe1e7fafec58b902b1171f67
XiaoyongNI <xiaoyongn@126.com>
2021-08-31 18:24:13 +0200

    change GPU notation

--- EKF.py
+++ EKF.py
@@ -10,10 +10,10 @@ sys.path.insert(1, path_model)
 from model import getJacobian
 
 if torch.cuda.is_available():
-    cuda0 = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
+    dev = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 else:
-   cuda0 = torch.device("cpu")
+   dev = torch.device("cpu")
    print("Running on the CPU")
 
 class ExtendedKalmanFilter:
