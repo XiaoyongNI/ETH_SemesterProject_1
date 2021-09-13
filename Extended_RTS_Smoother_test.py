47fb81dd922ed9616ae084523b77a18524d8658b
XiaoyongNI <xiaoyongn@126.com>
2021-09-13 11:31:46 +0200

    add RunTime calculation

--- Extended_RTS_Smoother_test.py
+++ Extended_RTS_Smoother_test.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import time
 from EKF import ExtendedKalmanFilter
 from Extended_RTS_Smoother import Extended_rts_smoother
 from Extended_data import N_T
@@ -17,20 +17,23 @@ def S_Test(SysModel, test_input, test_target, modelKnowledge = 'full'):
     EKF.InitSequence(SysModel.m1x_0, SysModel.m2x_0)
     ERTS = Extended_rts_smoother(SysModel, modelKnowledge)
     ERTS_out = torch.empty([N_T, SysModel.m, SysModel.T_test])
-
+    start = time.time()
     for j in range(0, N_T):
 
         EKF.GenerateSequence(test_input[j, :, :], EKF.T_test)
         ERTS.GenerateSequence(EKF.x, EKF.sigma, ERTS.T_test)
         MSE_ERTS_linear_arr[j] = loss_rts(ERTS.s_x, test_target[j, :, :]).item()
         ERTS_out[j,:,:] = ERTS.s_x
-
+    end = time.time()
+    t = end - start
     MSE_ERTS_linear_avg = torch.mean(MSE_ERTS_linear_arr)
     MSE_ERTS_dB_avg = 10 * torch.log10(MSE_ERTS_linear_avg)
 
     print("Extended RTS Smoother - MSE LOSS:", MSE_ERTS_dB_avg, "[dB]")
+    # Print Run Time
+    print("Inference Time:", t)
 
-    return [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out]
+    return [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out,t]
 
 
 
--- main_linear.py
+++ main_linear.py
@@ -1,8 +1,6 @@
 import torch
 torch.pi = torch.acos(torch.zeros(1)).item() * 2 # which is 3.1415927410125732
 import torch.nn as nn
-from EKF_test import EKFTest
-from Extended_RTS_Smoother_test import S_Test
 from Linear_sysmdl import SystemModel
 from Extended_data import DataGen,DataLoader,DataLoader_GPU, Decimate_and_perturbate_Data,Short_Traj_Split
 from Extended_data import N_E, N_CV, N_T, F, H, F_rotated, H_rotated, T, T_test, m1_0, m2_0, m, n
