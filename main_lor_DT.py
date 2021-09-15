f2cc4ddcd659963311f4034564cf946c2e86840c
XiaoyongNI <xiaoyongn@126.com>
2021-09-15 14:40:33 +0200

    change training dataset

--- main_lor_DT.py
+++ main_lor_DT.py
@@ -52,7 +52,7 @@ sequential_training = False
 path_results = 'ERTSNet/'
 DatafolderName = 'Simulations/Lorenz_Atractor/data/T2000_NT100' + '/'
 
-r2 = torch.tensor([1e-3])
+r2 = torch.tensor([1])
 # r2 = torch.tensor([100, 10, 1, 0.1, 0.01])
 r = torch.sqrt(r2)
 vdB = -20 # ratio v=q2/r2
@@ -61,32 +61,33 @@ v = 10**(vdB/10)
 q2 = torch.mul(v,r2)
 q = torch.sqrt(q2)
 
-r2optdB = torch.tensor([32])
+r2optdB = torch.tensor([3.0103])
 ropt = torch.sqrt(10**(-r2optdB/10))
-q2optdB = torch.tensor([48])
+q2optdB = torch.tensor([18.2391])
 qopt = torch.sqrt(10**(-q2optdB/10))
 print("1/r2 [dB]: ", 10 * torch.log10(1/r[0]**2))
 print("1/q2 [dB]: ", 10 * torch.log10(1/q[0]**2))
 
 # traj_resultName = ['traj_lor_KNetFull_rq1030_T2000_NT100.pt']#,'partial_lor_r4.pt','partial_lor_r5.pt','partial_lor_r6.pt']
-dataFileName = ['data_lor_v20_rq3050_T2000.pt']#,'data_lor_v20_r1e-2_T100.pt','data_lor_v20_r1e-3_T100.pt','data_lor_v20_r1e-4_T100.pt']
-KFRTSResultName = 'KFRTS_partialh_rq3050_T2000' 
+dataFileName = ['data_lor_v20_rq020_T2000.pt']#,'data_lor_v20_r1e-2_T100.pt','data_lor_v20_r1e-3_T100.pt','data_lor_v20_r1e-4_T100.pt']
+KFRTSResultName = 'KFRTS_partialh_rq020_T2000' 
 
 #Generate and load data DT case
-# sys_model = SystemModel(f, q[0], h, r[0], T, T_test, m, n,"Lor")
-# sys_model.InitSequence(m1x_0, m2x_0)
-# print("Start Data Gen")
-# # T = 2000
-# DataGen(sys_model, DatafolderName + dataFileName[0], T, T_test)  
+sys_model = SystemModel(f, q[0], h, r[0], T, T_test, m, n,"Lor")
+sys_model.InitSequence(m1x_0, m2x_0)
+TraindataFileName = 'data_lor_rq020_TrainT100.pt'
+print("Start Data Gen")
+DataGen(sys_model, DatafolderName + TraindataFileName, T, T_test)  
 print("Data Load")
 print(dataFileName[0])
-[train_input, train_target, cv_input, cv_target, test_input, test_target] =  torch.load(DatafolderName + dataFileName[0],map_location=dev)  
+[_,_, cv_input, cv_target, test_input, test_target] =  torch.load(DatafolderName + dataFileName[0],map_location=dev)  
+[train_input,train_target,_, _, _, _] =  torch.load(DatafolderName + TraindataFileName,map_location=dev)  
 print("trainset size:",train_target.size())
 print("cvset size:",cv_target.size())
 print("testset size:",test_target.size())
 for rindex in range(0, len(ropt)):
    # Model with full info
-   sys_model = SystemModel(f, q[0], h, r[0], T, T_test,m,n)
+   sys_model = SystemModel(f, qopt, h, r[0], T, T_test,m,n)
    sys_model.InitSequence(m1x_0, m2x_0)
 
    # Model with partial Info
@@ -107,11 +108,11 @@ for rindex in range(0, len(ropt)):
    # [MSE_PF_linear_arr_partial, MSE_PF_linear_avg_partial, MSE_PF_dB_avg_partial, PF_out_partial, t_PF] = PFTest(sys_model_partialh, test_input, test_target, init_cond=None)
    # print(f"MSE PF H NL: {MSE_PF_dB_avg_partial} [dB] (T = {T_test})")
    #Evaluate RTS true
-   # print("Evaluate RTS true")
-   # [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out] = S_Test(sys_model, test_input, test_target)
-   # #Evaluate RTS partialh optr
-   # print("Evaluate RTS partial")
-   # [MSE_ERTS_linear_arr_partialoptr, MSE_ERTS_linear_avg_partialoptr, MSE_ERTS_dB_avg_partialoptr, ERTS_out_partialoptr] = S_Test(sys_model_partialh, test_input, test_target)
+   print("Evaluate RTS true")
+   [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out] = S_Test(sys_model, test_input, test_target)
+   #Evaluate RTS partialh optr
+   print("Evaluate RTS partial")
+   [MSE_ERTS_linear_arr_partialoptr, MSE_ERTS_linear_avg_partialoptr, MSE_ERTS_dB_avg_partialoptr, ERTS_out_partialoptr] = S_Test(sys_model_partialh, test_input, test_target)
    
    
    # Save results
@@ -154,8 +155,8 @@ for rindex in range(0, len(ropt)):
    RTSNet_Pipeline = Pipeline(strTime, "RTSNet", "RTSNet")
    RTSNet_Pipeline.setssModel(sys_model)
    RTSNet_Pipeline.setModel(RTSNet_model)
-   RTSNet_Pipeline.setTrainingParams(n_Epochs=100, n_Batch=20, learningRate=1e-4, weightDecay=1e-6)
-   RTSNet_Pipeline.model = torch.load('ERTSNet/best-model_DTfull_rq3050_T2000.pt',map_location=dev)
+   RTSNet_Pipeline.setTrainingParams(n_Epochs=1000, n_Batch=30, learningRate=1e-4, weightDecay=1e-6)
+   # RTSNet_Pipeline.model = torch.load('ERTSNet/best-model_DTfull_rq3050_T2000.pt',map_location=dev)
    [MSE_cv_linear_epoch, MSE_cv_dB_epoch, MSE_train_linear_epoch, MSE_train_dB_epoch] = RTSNet_Pipeline.NNTrain(sys_model, cv_input, cv_target, train_input, train_target, path_results)
    ## Test Neural Network
    [MSE_test_linear_arr, MSE_test_linear_avg, MSE_test_dB_avg,rtsnet_out,RunTime] = RTSNet_Pipeline.NNTest(sys_model, test_input, test_target, path_results)
